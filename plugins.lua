3a9edffd481d5ab61d0f8764837d124f61c1a515
Sophie Hirn <sophie.hirn@wyvernscale.com>
2024-02-03 11:29:17 +0100

    Changed semantics of print commands

--- plugins.lua
+++ plugins.lua
@@ -277,21 +277,26 @@ end
 
 function m.core_print(state)
 	table.insert(state.cmds.range_local, {"^$", function(m, a, b)
-		state:cmd(tostring(a) .. "," .. tostring(b) .. "l")
-	end, "print code listing (alias for `l` command)"})
-
-	table.insert(state.cmds.range_local, {"^l$", function(m, a, b)
 		local lines = {}
 		for i = a, b do lines[i] = true end
 		state.curr:print(lines)
 	end, "print code listing (use the print pipeline)"})
 
-	table.insert(state.cmds.range_local, {"^n$", function(m, a, b)
+	table.insert(state.cmds.range_line, {"^l$", function(m, a, b)
+		local lines = {}
+		for i = a, b do lines[i] = true end
+		state.curr:print(lines)
+		state.curr:seek(b)
+	end, "print code listing (use the print pipeline)"})
+
+	table.insert(state.cmds.range_line, {"^n$", function(m, a, b)
 		state.curr:inspect(function(n, l) print(n, l.text) end, a, b)
+		state.curr:seek(b)
 	end, "print lines with line numbers"})
 
-	table.insert(state.cmds.range_local, {"^p$", function(m, a, b)
+	table.insert(state.cmds.range_line, {"^p$", function(m, a, b)
 		state.curr:inspect(function(n, l) print(l.text) end, a, b)
+		state.curr:seek(b)
 	end, "print raw lines"})
 
 end
